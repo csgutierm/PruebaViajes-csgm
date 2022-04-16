9f515d27335ae852b9b65ac48e3bbd23564a24a9
csgutier <csgutier@gmail.com>
2022-04-16 18:28:48 -0400

    fix navbar dont showing initial configuration - add initial script

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,6 +5,10 @@ $(document).ready(function () {
     // del documento se activa al detectarse un "scroll" en la ventana
     // se utiliza en este caso para cambiar el fondo del nav de transparente a no transparente
     // los primeros 100 scrolls es transparente y luego no 
+
+    // clase inicial
+    document.getElementById("navTest").className = "navbar navbar-expand-lg navbar-dark bg-transparent mb-lg-3 fixed-top";
+
     window.onscroll = function() {myFunction()};
 
     function myFunction() {
